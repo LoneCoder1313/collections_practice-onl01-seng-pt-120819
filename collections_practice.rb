2b9c55945ebf7d9e074425866e3043d50b4d0574
Ricky dildy <rdildy92@gmail.com>
2019-12-23 02:07:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,7 @@ def kesha_maker(array)
   end
 end
 
-# def find_a(array)
-#   array.find_all do |word|
-#     word[0] == "a"
-#   end
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
