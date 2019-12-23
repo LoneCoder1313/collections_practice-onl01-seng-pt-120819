21b553092928957b79fbcecfe44f9aa3615c72e0
Ricky dildy <rdildy92@gmail.com>
2019-12-23 01:39:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,7 +21,8 @@ end
 def swap_elements(array)
    array.each_with_index do |array, i|
      if array.index = [0,1,2] 
-       array[1,2].reverse
+       array.destination_with_index
+       puts array[]
 end 
 end 
       
