2f92382da2f727d64252e6d9d10d80d90c89833a
Ricky dildy <rdildy92@gmail.com>
2019-12-23 02:12:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,8 +19,8 @@ def sort_array_char_count(array)
 end 
 
 def swap_elements(array)
-      array_destination_index do 
-         array[1], array[2]<=> array[2], array[1]
+     if array_destination_index do 
+         array[1], array[2] <=> array[2], array[1]
 end 
   end 
      array=["blake",  "scott", "ashley"]
